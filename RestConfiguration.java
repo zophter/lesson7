930d3816850faa104bab017e33ee652356bd5b6a
zophter <100192111+zophter@users.noreply.github.com>
2022-09-04 15:19:37 +0300

    Update RestConfiguration.java

--- RestConfiguration.java
+++ RestConfiguration.java
@@ -1,1 +1,11 @@
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.client.RestTemplate;
 
+@Configuration
+public class RestConfiguration {
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+}
